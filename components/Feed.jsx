5cd1a904dd3cdab6fac05abe98aec527ad017624
nicoladelbianco <nicola.delbianco97@gmail.com>
2023-06-02 16:53:56 +0200

    fix PromptCardList return

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,13 +6,15 @@ import PromptCard from "./PromptCard";
 const PromptCardList = ({ data, handleTagClick }) => {
     return (
         <div className="mt-16 prompt_layout">
-            {data.map((post) => (
-                <PromptCard
-                    key={post._id}
-                    post={post}
-                    handleTagClick={handleTagClick}
-                />
-            ))}
+            {data.map((post) => {
+                return (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+                    />
+                );
+            })}
         </div>
     );
 };
